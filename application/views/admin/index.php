601c65a2237ae55d590ce64096339ab080147795
ekky2412 <ekky.raharjo@gmail.com>
2021-02-20 12:49:31 +0700

    edit tampilan tabel

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -4,41 +4,43 @@
         <h3>Berita Milikmu :</h3>
         <a class="ml-auto btn btn-success" href="<?= base_url() ?>admin/tambah">Tambah Data</a>
     </div>
-    <table class="table table-striped table-bordered" id="table">
-        <thead>
-            <th>Judul Berita</th>
-            <th>Isi Berita</th>
-            <th>Status Aktif</th>
-            <th>Aksi</th>
-        </thead>
-        <tbody>
-            <?php
-            foreach ($data as $d) {
-            ?>
-            <tr>
-                <td><?= $d['judul'] ?></td>
-                <td><?= $d['pesan'] ?></td>
-                <td>
-                    <?php
-                    if ($d['status_aktif'] == '1') {
-                    ?>
-                        <a class="btn btn-success" href="<?= base_url() ?>admin/editAktif/<?= $d['id'] ?>/0">Aktif</a>
-                    <?php
-                    } else {
-                    ?>
-                    <a class="btn btn-danger" href="<?=base_url()?>admin/editAktif/<?=$d['id']?>/1">Tidak Aktif</a>
-                    <?php
-                    }
-                    ?>
-                </td>
-                <td>
-                    <a class="btn btn-warning" href="<?=base_url()?>admin/edit/<?=$d['id']?>">Edit</a>
-                    <a class="btn btn-danger" href="<?=base_url()?>admin/hapus/<?=$d['id']?>" onclick="return confirm('Apakah anda ingin menghapus data ini?')">Hapus</a>
-                </td>
-            </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
+    <div class="col-md-12">
+        <table class="table table-striped table-bordered" id="table">
+            <thead>
+                <th>Judul Berita</th>
+                <th>Isi Berita</th>
+                <th>Status Aktif</th>
+                <th>Aksi</th>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($data as $d) {
+                ?>
+                    <tr>
+                        <td><?= $d['judul'] ?></td>
+                        <td><?= $d['pesan'] ?></td>
+                        <td>
+                            <?php
+                            if ($d['status_aktif'] == '1') {
+                            ?>
+                                <a class="btn btn-success" href="<?= base_url() ?>admin/editAktif/<?= $d['id'] ?>/0">Aktif</a>
+                            <?php
+                            } else {
+                            ?>
+                                <a class="btn btn-danger" href="<?= base_url() ?>admin/editAktif/<?= $d['id'] ?>/1">Tidak Aktif</a>
+                            <?php
+                            }
+                            ?>
+                        </td>
+                        <td>
+                            <a class="btn btn-warning" href="<?= base_url() ?>admin/edit/<?= $d['id'] ?>">Edit</a>
+                            <a class="btn btn-danger" href="<?= base_url() ?>admin/hapus/<?= $d['id'] ?>" onclick="return confirm('Apakah anda ingin menghapus data ini?')">Hapus</a>
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </div>
